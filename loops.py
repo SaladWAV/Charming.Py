5a900237389730d6efe0a5ab5ee1a641acc3e9c7
Salad <virtueovervice92@gmail.com>
2024-06-26 21:23:06 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,12 @@
+# while loop
+i = 0
+while i < 3:
+    print("Meow")
+    i +=1
+
+# for loop
+for _ in range(3):
+    print("Meow")
+
+# simplifying this program without loops:
+print("meow\n" * 3, end ="")
---
+++ mario.py
@@ -0,0 +1,12 @@
+def main():
+
+# def vertical_block():
+# print("#\n" * 3, end = "")
+# vertical_block()
+
+    def square_block():
+        print("###\n" + "###\n" + "###\n")
+
+    square_block()
+
+main()
