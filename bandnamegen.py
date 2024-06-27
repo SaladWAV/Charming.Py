2032fc5cb39e175a8b07586b4f92f5a08bb47011
Salad <virtueovervice92@gmail.com>
2024-06-27 21:02:05 +0530

    Add files via upload

---
+++ bandnamegen.py
@@ -0,0 +1,4 @@
+pet_name = input("Pet Name? ")
+city_name = input("City name? ")
+
+print("Your band name can be:", pet_name + " " + city_name)
---
+++ calculator.py
@@ -0,0 +1,9 @@
+x = int(input("X? "))
+sign = input("Operation? ")
+y = int(input("Y? "))
+
+if sign := "-":
+    print(f"{x - y}")
+
+else:
+    print("am sorry i don't understand")
